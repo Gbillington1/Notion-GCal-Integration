c7be38e13e30f0836650f09b49b46ad76bec97bc
Graham Billington <Grahamhbillington@gmail.com>
2022-09-02 02:28:56 -0400

    cleaned data from notion in condensed object for use in google cal query in future

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ async function getActiveDeadlines() {
                                 property: "Status",
                                 select: {
                                     equals: "To Do"
-                                } 
+                                }
                             },
                             {
                                 property: "Status",
@@ -38,21 +38,42 @@ async function getActiveDeadlines() {
             }
         })
         return response;
-    } catch(err) {
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+async function getProjectName(id) {
+    try {
+        const response = await notion.pages.retrieve({ page_id: id });
+        return response.properties.Name.title[0].text.content;
+    } catch (err) {
         console.log(err)
     }
 }
 
 // print all deadlines that are "To Do" or "In Progress" to console
-;(async () => {
+; (async () => {
     const deadlines = await getActiveDeadlines();
 
-    let deadlinesNoTime = deadlines.results.map(deadline => {
-        if (!deadline.properties.Date.date.start.includes('T')) {
-            return deadline
-        }
-    })
+    let deadlinesNoTime = await Promise.all(deadlines.results.filter(deadline => {
+        return !deadline.properties.Date.date.start.includes('T')
+    }).map(async deadline => {
+            const projectTitle = await getProjectName(deadline.properties.Project.relation[0].id);
+
+            return {
+                id: deadline.id,
+                title: deadline.properties.Name.title[0].text.content,
+                created_time: deadline.created_time,
+                last_edited_time: deadline.last_edited_time,
+                project_id: deadline.properties.Project.relation[0].id,
+                project_title: projectTitle,
+            }
+    }))
 
     console.log(deadlinesNoTime)
+
+    // TODO: ensure that project_title matches event title in Google Calendar
+    // TODO: use deadlinesNoTime data to query google clanedar for class periods on that day. Return time that class starts
 })();
 
